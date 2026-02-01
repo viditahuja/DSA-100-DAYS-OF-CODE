31acd06fab89455ea3b1de0c6a3ee109637c4586
vaishnavijha <vyshnavi.k298@gmail.com>
2026-02-01 10:00:00 +0530

    Day 1 - DSA practice

---
+++ Day1/q1.c
@@ -0,0 +1,48 @@
+/*Insert an Element in an Array
+
+Problem: Write a C program to insert an element x at a given 1-based position pos in an array of n integers. Shift existing elements to the right to make space.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers (the array)
+- Third line: integer pos (1-based position)
+- Fourth line: integer x (element to insert)
+
+Output:
+- Print the updated array (n+1 integers) in a single line, space-separated
+
+Example:
+Input:
+5
+1 2 4 5 6
+3
+3
+
+Output:
+1 2 3 4 5 6
+
+Explanation: Insert 3 at position 3, elements [4,5,6] shift right*/
+
+#include <stdio.h>
+
+int main() {
+    int n, pos, x;
+    int a[100];
+
+    scanf("%d", &n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+
+    scanf("%d", &pos);
+    scanf("%d", &x);
+
+    for (int i = n; i >= pos; i--)
+        a[i] = a[i - 1];
+
+    a[pos - 1] = x;
+
+    for (int i = 0; i <= n; i++)
+        printf("%d ", a[i]);
+
+    return 0;
+}
