98e950b1110199a76d6b8a831fbabf9e8f7dc330
vaishnavijha <vyshnavi.k298@gmail.com>
2026-03-27 10:00:00 +0530

    Day 55 - DSA practice

---
+++ Day55/q2.c
@@ -0,0 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct TreeNode {
+    int val;
+    struct TreeNode* left;
+    struct TreeNode* right;
+} TreeNode;
+
+TreeNode* newNode(int val) {
+    TreeNode* node = (TreeNode*)malloc(sizeof(TreeNode));
+    node->val = val;
+    node->left = node->right = NULL;
+    return node;
+}
+
+TreeNode* buildTree(int arr[], int n) {
+    if (n == 0 || arr[0] == -1) return NULL;
+
+    TreeNode** queue = (TreeNode**)malloc(n * sizeof(TreeNode*));
+    int front = 0, rear = 0;
+
+    TreeNode* root = newNode(arr[0]);
+    queue[rear++] = root;
+
+    int i = 1;
+
+    while (i < n) {
+        TreeNode* curr = queue[front++];
+
+        if (arr[i] != -1) {
+            curr->left = newNode(arr[i]);
+            queue[rear++] = curr->left;
+        }
+        i++;
+
+        if (i < n && arr[i] != -1) {
+            curr->right = newNode(arr[i]);
+            queue[rear++] = curr->right;
+        }
+        i++;
+    }
+
+    return root;
+}
+
+void rightSideView(TreeNode* root) {
+    if (!root) return;
+
+    TreeNode* queue[1000];
+    int front = 0, rear = 0;
+
+    queue[rear++] = root;
+
+    while (front < rear) {
+        int size = rear - front;
+
+        for (int i = 0; i < size; i++) {
+            TreeNode* curr = queue[front++];
+
+            if (i == size - 1)
+                printf("%d ", curr->val);
+
+            if (curr->left)
+                queue[rear++] = curr->left;
+
+            if (curr->right)
+                queue[rear++] = curr->right;
+        }
+    }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    TreeNode* root = buildTree(arr, n);
+
+    rightSideView(root);
+
+    return 0;
+}
