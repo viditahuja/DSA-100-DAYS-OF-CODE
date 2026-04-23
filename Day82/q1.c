9f0afa5070834c8b2dbf32c2e4f733895f242acb
vaishnavijha <vyshnavi.k298@gmail.com>
2026-04-23 12:39:32 +0530

    Added Day82 - DSA practice

---
+++ Day82/q1.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+int lowerBound(int arr[], int n, int x) {
+    int low = 0, high = n - 1, ans = n;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] >= x) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+int upperBound(int arr[], int n, int x) {
+    int low = 0, high = n - 1, ans = n;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] > x) {
+            ans = mid;
+            high = mid - 1;
+        } else {
+            low = mid + 1;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    int n, x;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &x);
+
+    int lb = lowerBound(arr, n, x);
+    int ub = upperBound(arr, n, x);
+
+    printf("%d %d", lb, ub);
+
+    return 0;
+}
